837fa0d59d0f04e05fe4e36d22e4272f81fa90ba
Dorjee <rishav345@gmail.com>
2023-05-05 09:06:26 -0400

    creating the repositiory

---
+++ src/Components/PlaylistList/PlaylistList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Spotify from '../../util/Spotify';
+
+import Playlist  from '../Playlist/Playlist';
+
+
+class PlaylistList extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    this.setState({[]})
+}
+
+Spotify.getUserPlaylist(){
+
+}
+
+render (){
+    return (
+        <PlaylistList playlist = {this.Playlist} /> 
+    )
+}
